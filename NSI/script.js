8e0d9b7b63e616ba2f2ebdbfedaa9dabcf2d63e2
繧ｨ繧ｯ繧ｽ繧ｷ繧ｺ繝? <46537034+Exorcism0666@users.noreply.github.com>
2022-10-23 22:00:35 +0200

    Add files via upload

---
+++ NSI/script.js
@@ -0,0 +1,73 @@
+var Window;
+function valider1() {
+if (document.getElementById('serie1a').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider2() {
+if (document.getElementById('serie2b').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider3() {
+if (document.getElementById('serie3c').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider4() {
+if (document.getElementById('serie4b').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider5() {
+if (document.getElementById('serie5a').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider6() {
+if (document.getElementById('serie6b').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider7() {
+if (document.getElementById('serie7c').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider8() {
+if (document.getElementById('serie8b').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
+function valider9() {
+if (document.getElementById('serie9b').checked) {
+alert('Bonne réponse!!!')
+}
+else {
+alert('Réponse incorrecte.')
+}
+}
